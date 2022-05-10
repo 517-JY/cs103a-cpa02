05e1a30581084f9fc2ddf943fe1924d9f2aa865e
Jiayin Li <jiayin.li007@outlook.com>
2022-05-10 15:09:26 -0400

    add startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export mongodb_URI='mongodb://localhost:27017/cs103a_L36'
+export mongodb_URI='mongodb+srv://jiayinli007:12345@cluster0.hcayx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 echo "connecting to $mongodb_URI"
 nodemon
