53496e2357effdbcb27165c5aab98d5e25efa394
Jiayin Li <jiayin.li007@outlook.com>
2022-05-10 16:16:03 -0400

    env and dataset setting

--- startup.ps1
+++ startup.ps1
@@ -1,2 +1,2 @@
-$Env.mongodb_URI = 'mongodb://localhost:27017/cs103a_L36'
+$Env.mongodb_URI = 'mongodb+srv://jiayinli007:12345@cluster0.hcayx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 npm start
